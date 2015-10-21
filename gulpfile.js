7f11a124cb77e31ea9408115580b2711bbb5a5ff
Giordan Battaglin <gio@giordan.ca>
2015-10-21 16:10:29 -0400

    completed addition of plugins including browsersync and finalized watch file, now runs in gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -14,18 +14,31 @@ gulp.task('styles', function(){
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
 		.pipe(autoprefixer('last 2 versions', 'ie8'))
-		.pipe(gulp.dest('styles/'));
+		.pipe(gulp.dest('styles/'))
+		.pipe(reload({ stream: true }));
 });
 
 gulp.task('js', function(){
 	return gulp.src('scripts/*.js')
 		.pipe(jshint())
-		.pipe(jshint.reporter('jshint-stylish'));
+		.pipe(jshint.reporter('jshint-stylish'))
+		.pipe(reload({ stream: true }));
+});
+
+gulp.task('bs-task', function(){
+	browserSync.init({
+		server:{
+			baseDir: './'
+		}
+	});
 });
 
 //watch task
 
 gulp.task('watch', function (){
 	gulp.watch('styles/*.scss', ['styles']);
-	gulp.watch('scripts/*.js', ['js'])
+	gulp.watch('scripts/*.js', ['js']);
+	gulp.watch('*.html', reload);
 });
+
+gulp.task('default', ['bs-task', 'styles', 'js', 'watch']);
