1edbc6ceb25bf89c2ce7ecfee9468e33a9ed73cf
Giordan Battaglin <gio@giordan.ca>
2015-10-21 15:15:15 -0400

    updated with jshint

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
-		concat = require('gulp-concat');
+		concat = require('gulp-concat'),
+		jshint = require('gulp-jshint');
 
 //styles task
 
@@ -9,4 +10,17 @@ gulp.task('styles', function(){
 		.pipe(sass().on('error', sass.logError))
 		.pipe(concat('style.css'))
 		.pipe(gulp.dest('styles/'));
+});
+
+gulp.task('js', function(){
+	return gulp.src('scripts/*.js')
+		.pipe(jshint())
+		.pipe(jshint.reporter('jshint-stylish'));
+});
+
+//watch task
+
+gulp.task('watch', function (){
+	gulp.watch('styles/*.scss', ['styles']);
+	gulp.watch('scripts/*.js', ['js'])
 });
